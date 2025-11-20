c929272537bb86bde341575d2e6a0f201d1718a5
DekelAH <54857125+DekelAH@users.noreply.github.com>
2025-11-20 11:00:25 +0200

    server update

--- server.js
+++ server.js
@@ -9,34 +9,34 @@ import { authRoutes } from './api/auth/auth.routes.js'
 
 const app = express()
 
+app.use(express.json())
+app.use(cookieParser())
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve('public')))
+} else {
+    const corsOptions = {
+
+        origin: [
+            'http://127.0.0.1:5173',
+            'http://localhost:5173',
+        ],
+
+        credentials: true
+    }
+    app.use(cors(corsOptions))
+}
+
+app.use('/api/bug', bugRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
 
 const port = process.env.PORT || 3030
 app.listen(port, () => {
 
     loggerService.info(`Server listening on port http://127.0.0.1:${port}/`)
-    console.log(`App listening on port ${port}!`) 
+    console.log(`App listening on port ${port}!`)
 })
-
-//* --------------- Config ---------------
-
-const corsOptions = {
-
-    origin: [
-        'http://127.0.0.1:5173',
-        'http://localhost:5173',
-    ],
-
-    credentials: true
-}
-
-app.use(cors(corsOptions))
-app.use(express.static('public'))
-app.use(express.json())
-app.use(cookieParser())
-app.use('/api/bug', bugRoutes)
-app.use('/api/user', userRoutes)
-app.use('/api/auth', authRoutes)
-
 
 
 
